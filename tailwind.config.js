0d4c76c54cb39e003c52fbd050a166dd37dc1877
Adrian Ozuna <adrian.gab.ozuna@outlook.com>
2023-11-29 16:16:34 -0400

    Added preline and tailwind forms plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "node_modules/preline/dist/*.js",
+  ],
   theme: {
     fontFamily: {
       sans: ["Poppins", "sans-serif"],
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require("preline/plugin"),
+  ],
 };
 
