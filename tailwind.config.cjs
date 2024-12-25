89310c5621a3cca3b7418401bdc4430fa58abccf
Adrian Ozuna <adrianozuna133@gmail.com>
2024-12-25 16:07:23 -0400

    Fixes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ export default {
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
     "./node_modules/preline/preline.js",
-  ],
+  ],  
   darkMode: "class",
   theme: {
     fontFamily: {
